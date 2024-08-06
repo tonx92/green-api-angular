189e13b5464f9b73dcf4a82c6eccff7f566e9cee
Anton Polyakov <a.polyakov@pandarium.pro>
2024-08-06 22:17:18 +0300

    builded project

--- src/app/shared/models/message.ts
+++
@@ -1,7 +0,0 @@
-export class Message {
-    idMessage: string
-
-    constructor(data:any){
-        this.idMessage = data.idMessage || null
-    }
-}
--- src/environments/environment.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: true,
-  apiUrl:'https://1103.api.green-api.com'
-};
